7631087fe42c9986267bad92e84542d72cab548f
RutFlo666 <154447740+RutFlo666@users.noreply.github.com>
2025-04-11 14:57:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You can start use colculator:)");
+            Console.Write("Enter the first namber:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter the second namber:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero!");
+                        return;
+                    }
+                    break;
+            }
+
+
+            Console.WriteLine($"Result: {num1} {op} {num2} = {result}");
+        }
+    }
+}
